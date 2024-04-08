2e23509fb8c13b75f3c0f4c948043fced96a3e3e
Byonghwan Son <byonghwan@trial-net.co.kr>
2024-04-08 18:49:10 +0900

    몽고 DB 연결한 NEST

--- chapter8/blog/src/app.module.ts
+++ chapter8/blog/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
-import { BlogFileRepository } from './blog.repository';
+import {BlogFileRepository, BlogMongoRepository} from './blog.repository';
+import {MongooseModule} from "@nestjs/mongoose";
+import {Blog, BlogSchema} from "./blog.mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://127.0.0.1:27017/blog?retryWrites=true&loadBalanced=false&connectTimeoutMS=10000'
+    ),
+    MongooseModule.forFeature([
+      {
+        name: Blog.name,
+        schema: BlogSchema,
+      }
+    ])
+  ],
   controllers: [BlogController],
-  providers: [BlogService, BlogFileRepository],
+  providers: [BlogService, BlogMongoRepository],
 })
 export class AppModule {}
--- chapter8/blog/src/blog.service.ts
+++ chapter8/blog/src/blog.service.ts
@@ -1,31 +1,60 @@
 import { PostDto } from './blog.model';
-import { BlogFileRepository } from './blog.repository';
+import {BlogFileRepository, BlogMongoRepository} from './blog.repository';
 import { Injectable } from '@nestjs/common';
+import {Blog} from "./blog.mongoose";
+
+// @Injectable()
+// export class BlogService {
+//   //posts: PostDto[] = []
+//   //blogRepository: BlogRepository;
+//
+//   constructor(private blogRepository: BlogFileRepository) {}
+//
+//   async getAllPosts(): Promise<PostDto[]> {
+//     return await this.blogRepository.getAllPost()
+//   }
+//
+//   createPost(postDto : PostDto) : void {
+//     this.blogRepository.createPost(postDto)
+//   }
+//
+//   async getPost(id: string): Promise<PostDto> {
+//     return await this.blogRepository.getPost(id);
+//   }
+//
+//   delete(id: string): void {
+//     this.blogRepository.deletePost(id);
+//   }
+//
+//   updatePost(id: string, postDto: PostDto) : void {
+//     this.blogRepository.updatePost(id, postDto)
+//   }
+// }
 
 @Injectable()
 export class BlogService {
   //posts: PostDto[] = []
   //blogRepository: BlogRepository;
 
-  constructor(private blogRepository: BlogFileRepository) {}
+  constructor(private blogRepository: BlogMongoRepository) {}
 
-  async getAllPosts(): Promise<PostDto[]> {
+  async getAllPosts(): Promise<Blog[]> {
     return await this.blogRepository.getAllPost()
   }
 
-  createPost(postDto : PostDto) : void {
-    this.blogRepository.createPost(postDto)
+  async createPost(blog: Blog): Promise<void> {
+    await this.blogRepository.createPost(blog)
   }
 
-  async getPost(id: string): Promise<PostDto> {
+  async getPost(id: string): Promise<Blog> {
     return await this.blogRepository.getPost(id);
   }
 
-  delete(id: string): void {
-    this.blogRepository.deletePost(id);
+  async delete(id: string): Promise<void> {
+    await this.blogRepository.deletePost(id);
   }
 
-  updatePost(id: string, postDto: PostDto) : void {
-    this.blogRepository.updatePost(id, postDto)
+  async updatePost(id: string, postDto: Blog): Promise<void> {
+    await this.blogRepository.updatePost(id, postDto)
   }
 }
